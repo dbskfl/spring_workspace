a3d1a09f8bd9f22837e710d742885d981e2b9893
lllyyynnn0201 <lllyyynnn0201@gmail.com>
2019-06-12 13:25:13 +0900

    테스트

---
+++ main/resources/static/resources/js/index.js
@@ -0,0 +1,49 @@
+var app = (function(){    //var app- json객체
+   let init=function(){
+      login_form();
+   }
+  let login_form = function(){
+   var wrapper = document.querySelector('#wrapper');  
+   wrapper.innerHTML = '<form action="/action_page.php">'
+   +'  First name:<br>'
+   +'  <input type="text" name="firstname" value="Mickey">'
+   +'  <br>'
+   +'  Last name:<br>'
+   +'  <input type="text" name="lastname" value="Mouse">'
+   +'  <br><br>'
+   +'  <input type="submit" value="Login">'
+   +'</form> '
+
+   //
+   let join_form = function(){
+      var wrapper = document.querySelector('#wrapper');  
+      wrapper.innerHTML = '<form action="/action_page.php">'
+      '  id:<br>'
+      '  pw:<br>'
+      '  name:<br>'
+      '  phone:<br>'
+      '<input type="button" value="회원가입">'    
+      '</form> '
+//
+
+
+   var btn = document.querySelector('#btn');
+   var target = document.getElementById('target');
+   btn.addEventListener('click', function(){
+     target.innerHTML='';
+   });
+   var btn2 = document.querySelector('#btn2');
+   btn2.addEventListener('click',function(){
+     var temp = document.createTextNode('새로추가됨');
+     target.appendChild(temp);
+    });
+//
+    var btn3 = document.querySelector('#btn3');
+    btn3.addEventListener('click',function(){
+       target.appendChild(temp);
+    });
+//
+
+   }
+   return {init : init};       //클로저 
+})();   

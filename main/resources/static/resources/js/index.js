2e42e827ace809fdeee99b5ceb6a5f80269d67b0
lllyyynnn0201 <lllyyynnn0201@gmail.com>
2019-06-12 14:30:06 +0900

    14:30

--- main/resources/static/resources/js/index.js
+++ main/resources/static/resources/js/index.js
@@ -1,54 +1,41 @@
-var app = (function(){    //var app- json객체
+var app = (function(){
    let init=function(){
-      login_form();
+       login_form();
    }
-  let login_form = function(){
-   var wrapper = document.querySelector('#wrapper');  
-   wrapper.innerHTML = '<form action="/action_page.php">'
-   +'  First name:<br>'
-   +'  <input type="text" name="firstname" value="Mickey">'
-   +'  <br>'
-   +'  Last name:<br>'
-   +'  <input type="text" name="lastname" value="Mouse">'
-   +'  <br><br>'
-   +'  <input type="submit" value="Login">'
-   +'</form> '
-
-   //
+   let login_form = function(){
+       let wrapper = document.querySelector('#wrapper');
+       wrapper.innerHTML = '<form action="/action_page.php">'
+       +'  First name:<br>'
+       +'  <input type="text" name="firstname" value="Mickey">'
+       +'  <br>'
+       +'  Last name:<br>'
+       +'  <input type="text" name="lastname" value="Mouse">'
+       +'  <br><br>'
+       +'  <input id="login-btn" type="button" value="LOGIN">'
+       +'  <input id="join-btn" type="button" value="JOIN">'
+       +'</form> ';
+       let join_btn = document.querySelector('#join-btn');
+       join_btn.addEventListener('click',function(){
+           join_form();
+       });
+       
+   }
    let join_form = function(){
-      var wrapper = document.querySelector('#wrapper');  
-      wrapper.innerHTML = '<form action="/action_page.php">'
-      '  id:<br>'
-      '  <input type="text" name="" >'
-      '  <br>'
-      '  pw:<br>'
-      '  <input type="num" name="" >'
-      '  name:<br>'
-      '  <input type="text" name="" >'
-      '  phone:<br>'
-      '  <input type="num" name="" >'
-      '  <br><br>'
-      ' <input type="button" value="회원가입">'    
-      '</form> '
-//
-
-   var btn = document.querySelector('#btn');
-   var target = document.getElementById('target');
-   btn.addEventListener('click', function(){
-     target.innerHTML='';
-   });
-   var btn2 = document.querySelector('#btn2');
-   btn2.addEventListener('click',function(){
-     var temp = document.createTextNode('새로추가됨');
-     target.appendChild(temp);
-    });
-//
-    var btn3 = document.querySelector('#btn3');
-    btn3.addEventListener('click',function(){
-       target.appendChild(temp);
-    });
-//
-
+       let wrapper = document.querySelector('#wrapper');
+       wrapper.innerHTML = '<form>'
+       +'	ID<br>'
+       +'	<input type="text" name="id"><br>'
+       +'	PW<br>'
+       +'	<input type="password" name="pw"><br>'
+       +'	이름<br>'
+       +'	<input type="text" name="name"><br>'
+       +'	주민번호<br>'
+       +'	<input type="password" name="ssn"><br>'
+       +'	전화번호<br>'
+       +'	<input type="text" name="phone"><br><br>'
+       +'	<input id="btn3" type="submit" value="확인">'
+       +'	<input id="btn4" type="reset" value="취소">'
+       +'</form>';
    }
-   return {init : init};       //클로저 
-}});   
+   return {init : init};
+})();
